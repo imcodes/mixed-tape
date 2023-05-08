d747e478168c044f63719c40d356cd1bfa511d07
Michael Ifeanyi <worthsalive@gmail.com>
2023-05-08 14:36:13 +0100

    The app comming together with styles. Awesome!

---
+++ src/Controller/ApiSongController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class ApiSongController extends AbstractController
+{
+    #[Route('api/songs/{id<\d+>}')]
+	public function getSong(int $id, LoggerInterface $logger): Response
+    {
+       //TODO: Query the database for the song data
+       $song = [
+        'id' => $id,
+        'name' => 'Waterfalls',
+        'url'   =>  'https://symfonycasts.s3.amazonaws.com/sample.mp3'
+       ];
+
+       $logger->info('Returning the json response for song {song}', ['song' => $id]);
+
+       return new JsonResponse($song); //or return $this->json($song); //also works
+    }
+}
--- src/Controller/TapeController.php
+++ src/Controller/TapeController.php
@@ -2,23 +2,39 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class TapeController
+class TapeController extends AbstractController
 {
-    #[Route('/')]
+    #[Route('/', name: 'app_home')]
 	public function homepage(): Response
     {
-        return new Response('Hello dear! You are welcom to Symfny world');
+        $songData = [
+            ['track' => 'Gangsta\'s Paradise', 'artist' => 'Coolio'],
+            ['track' => 'Waterfalls', 'artist' => 'TLC'],
+            ['track' => 'Creep', 'artist' => 'Radiohead'],
+            ['track' => 'Kiss from a Rose', 'artist' => 'Seal'],
+            ['track' => 'On Bended Knee', 'artist' => 'Boyz II Men'],
+            ['track' => 'Fantasy', 'artist' => 'Mariah Carey'],
+        ];
+        $title = "PB & Jams";
+
+        return $this->render('tape/homepage.html.twig',[
+            'title' => $title,
+            'songs' => $songData
+        ]);
     }
 
-    #[Route('/browse/{slug}')]
+    #[Route( '/browse/{slug}', name: 'app_browse' )]
     public function browse($slug = null): Response
     {
         $title = ucwords(str_replace('-',' ',$slug));
-        $res = $slug ? 'Genre: '.$title : 'You are browsing the Music List Index page';
-        return new Response($res);
+        $genre = $slug ? $title : null;
+        return $this->render('tape/browse.html.twig',[
+            'genre' => $genre,
+        ]);
     }
 
 
